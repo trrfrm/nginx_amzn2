034ff0a2eb4ce56117a9f27c89bf919933bdc996
nykzaR <nykzar@outlook.com>
2024-01-31 05:51:04 +0530

    added files

---
+++ datasources.tf
@@ -0,0 +1,19 @@
+# data "aws_subnets" "subnets" {
+#   filter {
+#     name   = "tag:Name"
+#     values = var.subnet_tags
+#   }
+# }
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners      = [ "amazon" ]
+  filter {
+    name      = "name"
+    values    = [ "amzn2-ami-hvm-*-x86_64-gp2" ]
+  }
+  filter {
+    name      = "virtualization-type"
+    values    = [ "hvm" ]
+  }
+}
